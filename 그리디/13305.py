ec582adc56caab59d1dca8175b9fd9ffca3875c4
asd3638 <asd36384@gmail.com>
2022-02-28 17:48:35 +0900

    solve 브루트포스

---
+++ 그리디/13305.py
@@ -0,0 +1,10 @@
+# 입력
+city = int(input())
+city_len = list(map(int, input().split()))
+oil_price = list(map(int, input().split()))
+
+available = 0
+
+for price, i in enumerate(oil_price):
+    
+
--- 그리디/20921.py
+++ 그리디/20921.py
@@ -1,6 +1,17 @@
 n, k = map(int, input().split())
 
-answer = [i for i in range(1, n + 1)]
-print(answer.pop(k), end=' ')
-for i in answer:
-    print(i, end=' ')
+answer = [i for i in range(n, 0, -1)]
+value = sum(answer[1:])
+
+try:
+    for i in range(n - 1, 0, -1):
+        for j in range(n - 1, n - i - 1, -1):
+            if value == k:
+                raise NotImplementedError
+            answer.insert(j - 1, answer.pop(j))
+            value -= 1
+    for i in answer:
+        print(i, end=' ')
+except:
+    for i in answer:
+        print(i, end=' ')
